36fe1dbba98a8883c50b7b415c6e18dd67d2b4e1
Fredoxin <79151700+Fredoxin@users.noreply.github.com>
2023-07-29 22:38:25 +0200

    Update palindromes.js
    Passed the 2 last tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,27 +1,18 @@
-
-
 const palindromes = function (string) {
     let reversedString = "";
-    let punctuation = string.slice(string.length -1)
+    let newString = string.replace(/[^a-zA-Z1-9]/g, "").toLowerCase()
 
-for (let i = 0; i < string.length; i++) {
+    for (let i = 0; i < newString.length; i++) {
 
-       reversedString += string[string.length - 1 - i];
+      reversedString += newString[newString.length - 1 - i];
+      
+}
+
+return reversedString == newString;
 
 }
 
 
-if (string[string.length -1] == "." ||string[string.length -1] == "?" || string[string.length -1] == "!" ) {
-   reversedString = reversedString.slice(1)
-    reversedString += punctuation;
-} 
-console.log(reversedString)
-console.log(string)
-return reversedString.toLowerCase() == string.toLowerCase();
-
-}
-
-console.log(palindromes("Otto, otto"))
 
 
 // Do not edit below this line
